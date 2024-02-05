69a7994d1f4090a83ba4fb240ea1e1380e369f7b
yjw <yjw@atguigu.com>
2024-02-05 21:22:40 +0800

    init

---
+++ src/main/java/com/liuyi/springbootinit/controller/PostThumbController.java
@@ -0,0 +1,54 @@
+package com.liuyi.springbootinit.controller;
+
+import com.liuyi.springbootinit.common.BaseResponse;
+import com.liuyi.springbootinit.common.ErrorCode;
+import com.liuyi.springbootinit.common.ResultUtils;
+import com.liuyi.springbootinit.exception.BusinessException;
+import com.liuyi.springbootinit.model.dto.postthumb.PostThumbAddRequest;
+import com.liuyi.springbootinit.model.entity.User;
+import com.liuyi.springbootinit.service.PostThumbService;
+import com.liuyi.springbootinit.service.UserService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/liuyi/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.liuyi.springbootinit.mapper;
+
+import com.liuyi.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
